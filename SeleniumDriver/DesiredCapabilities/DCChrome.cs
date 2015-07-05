e40f99a50ad2b46b7f3657adecf43ad8cb47d9ff
romovs <romovs@gmail.com>
2015-07-05 19:08:06 +0300

    Removed unused code

--- SeleniumDriver/DesiredCapabilities/DCChrome.cs
+++ SeleniumDriver/DesiredCapabilities/DCChrome.cs
@@ -7,7 +7,7 @@ namespace CloudBeat.Oxygen
     {
         public DesiredCapabilities Create() 
         {
-			var dc = DesiredCapabilities.Chrome();
+			//var dc = DesiredCapabilities.Chrome();
 			ChromeOptions options = new ChromeOptions();
             // in Chrome 39+ SSLv3 fallback is disabled. we need to enable it back.
             // https://groups.google.com/a/chromium.org/forum/#!topic/security-dev/Vnhy9aKM_l4
