9d49354e0ab0753f77b1a8c3b387f02f92791685
romovs <romovs@gmail.com>
2016-01-31 10:37:37 +0200

    Build Oxygen on prepublish

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,28 @@
 var pkg = require('./package.json');
 
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-sync');
     grunt.loadNpmTasks('grunt-msbuild');
 
     var defaultTasks = [];
     defaultTasks.push('msbuild:oxygen');
-
+    defaultTasks.push('sync:main');
+    
     grunt.registerTask('default', defaultTasks);
 
     grunt.initConfig({
+        sync: {
+            main: {
+                files: [
+                    { 
+                        expand: true, 
+                        cwd: 'node_modules/oxygen/bin/Debug', src: ['**'], 
+                        dest: 'Oxygen' 
+                    }
+                ], 
+                verbose: true
+            },
+        },
         msbuild: {
             oxygen: {
                 src: ['node_modules/oxygen/Oxygen.csproj'],
