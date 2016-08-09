oxygenhq/oxygen

commit 35b99e68900fd068e0d729a5b5d38e393cf76205

Adjusted Oxygen to mobile and web modes.

ox_modules/module-web-dotnet.js
@@ -44,7 +44,8 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
     var _lastTransactionName;
 
 	// call ModuleInit
-	dispatcher.execute('web', 'moduleInit', argv);
+	if (dispatcher)
+		dispatcher.execute('web', 'moduleInit', argv);
 
     /**
      * @summary Initialize test settings and start correspondent Selenium server and browser.
@@ -301,7 +302,7 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
      */
     module.pause = function() { return handleStepResult(dispatcher.execute('web', 'pause', Array.prototype.slice.call(arguments))); };
     /**
-     * @summary Waits for new window to appear.
+     * @summary Waits for a popup window to appear.
      * @description <code>windowLocator</code> can be:
      *              <ul>
      *              <li><code>title=TITLE</code> - Wait for the first window which matches the 
@@ -310,12 +311,12 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
      *              </li>
      *              <li>An empty string - Wait for any new window to appear.</li>
      *              </ul>
-     * @function waitForWindow
+     * @function waitForPopUp
      * @param {String} windowLocator - A window locator.
      * @param {Integer} timeout - A timeout in milliseconds, after which the action will return with 
      *                           an error.
      */
-    module.waitForWindow = function() { return handleStepResult(dispatcher.execute('web', 'waitForWindow', Array.prototype.slice.call(arguments))); };
+    module.waitForPopUp = function() { return handleStepResult(dispatcher.execute('web', 'waitForPopUp', Array.prototype.slice.call(arguments))); };
     /**
      * @summary Selects a frame within the current window.
      * @description Available frame locators:

lib/oxygen.js
@@ -36,6 +36,7 @@ var Runner = module.exports.Runner = function () {
     var _isRunning = false;
     var _isInitializing = false;
     var _isDisposing = false;
+	var _mode = 'web';
     var childProc = null;
     var debugMode = false;
     var dbg = null;
@@ -56,7 +57,7 @@ var Runner = module.exports.Runner = function () {
     /*********************************
      * Public methods
      *********************************/
-    module.init = function (args, debugPort) {
+    module.init = function (args, mode, debugPort) {
         _isInitializing = true;
         // set up debugging port
         var forkExecArgv = { cwd: __dirname };
@@ -64,8 +65,20 @@ var Runner = module.exports.Runner = function () {
             debugMode = true;
             forkExecArgv = { execArgv: ['--debug-brk=' + debugPort] };
         }
+		if (mode && mode === 'mob')
+			_mode = 'mob';
         // apply configuration settings
-        args.push('--useFiber=' + useFiber);
+		// Fiber must be used in mobile mode but can't be used in web mode due to .NET interop issue
+		// so if Fiber is used, .NET dispatcher must not be loaded
+		if (_mode !== 'mob') {
+			args.push('--useFiber=' + useFiber);
+			args.push('--mode=web');
+		}
+		else {
+			args.push('--useFiber=true');	
+			args.push('--initDotNetDispatcher=false');
+			args.push('--mode=mob');
+		}
         args.push('--scriptContentLineOffset=' + module.getScriptContentLineOffset());
         
         // start script-boilerplate in a separate thread

lib/script-boilerplate.js
@@ -39,6 +39,8 @@ var globule = require('globule');
 var modules = {};
 var sandbox = {};
 var useFiber = (argv.useFiber === 'true');
+var initDotNetDispatcher = (argv.initDotNetDispatcher !== 'false');
+var mobileMode = (argv.mode === 'mob');
 var Fiber = useFiber ? require('fibers') : null;
 var scriptContentLineOffset = argv.scriptContentLineOffset;
 
@@ -60,6 +62,11 @@ Object.defineProperty(global, '__line', {
 	}
 });
 
+// log what mode the test is in
+if (mobileMode)
+	logger.debug('Starting in Mobile mode...');
+else
+	logger.debug('Starting in Web mode...');
 // initialize test execution context
 var ctx = {
     params: null,
@@ -70,33 +77,44 @@ var resultStore = {
     steps: []
 };
 
-// initialize .NET module dispatcher
-logger.debug('Initializing .NET module dispatcher - started');
 var homeDir = path.join(path.dirname(require.main.filename), '../');
-var dispatcher = new require('./dispatcher-dotnet.js')(homeDir, ctx, logger);
-logger.debug('Initializing .NET module dispatcher - ended');
+// initialize .NET module dispatcher
+var dispatcher = null;
+if (initDotNetDispatcher)
+{
+	logger.debug('Initializing .NET module dispatcher - started');
+	dispatcher = new require('./dispatcher-dotnet.js')(homeDir, ctx, logger);
+	logger.debug('Initializing .NET module dispatcher - ended');
+}
 
 var oxModulesPath = path.resolve(homeDir, "./ox_modules");
-var files = globule.find("module-*.js", { srcBase: oxModulesPath });
+var generalModuleFiles = globule.find("module-*-all.js", { srcBase: oxModulesPath });
+var dotnetModuleFiles = globule.find("module-*-dotnet.js", { srcBase: oxModulesPath });
+var fiberModuleFiles = globule.find("module-*-fiber.js", { srcBase: oxModulesPath });
 
 // initialize all modules
 logger.debug('Initializing modules - started');
-var regexMatchModuleName = /^module-(.+?)\.js$/;
+var regexMatchModuleName = /^module-(\w+?)-\w+?\.js$/;
 var err = null;
 var moduleName;
 try {
-    for (var i = 0; i < files.length; i++) {
-        var moduleFile = files[i];
-        // extract module name
-        var result = moduleFile.match(regexMatchModuleName);
-        moduleName = result[1];
-        var moduleType = require(path.join(oxModulesPath, moduleFile));
-        if (typeof moduleType === 'function') {
-            modules[moduleName] = sandbox[moduleName] = new moduleType(argv, ctx, resultStore, logger, dispatcher);
-        } else {
-            modules[moduleName] = sandbox[moduleName] = moduleType;
-        }
+	// load general modules
+    for (var i = 0; i < generalModuleFiles.length; i++) {
+		loadModule(generalModuleFiles[i]);
     }
+	// if .NET is enabled then load .NET based modules
+	if (initDotNetDispatcher)
+	{
+		for (var i = 0; i < dotnetModuleFiles.length; i++) {
+			loadModule(dotnetModuleFiles[i]);
+		}
+	}
+	//
+	if (useFiber) {
+		for (var i = 0; i < fiberModuleFiles.length; i++) {
+			loadModule(fiberModuleFiles[i]);
+		}
+	}
 } catch (e) {
     err = e;
     logger.debug('Error initializing module "' + moduleName + '": ' + e.message + EOL + e.stacktrace);
@@ -139,6 +157,18 @@ process.on('message', function (msg) {
     }
 });
 
+function loadModule(moduleFile) {
+	// extract module name
+	var result = moduleFile.match(regexMatchModuleName);
+	moduleName = result[1];
+	logger.debug('Loading module: ' + moduleName);
+	var moduleType = require(path.join(oxModulesPath, moduleFile));
+	if (typeof moduleType === 'function') {
+		modules[moduleName] = sandbox[moduleName] = new moduleType(argv, ctx, resultStore, logger, dispatcher);
+	} else {
+		modules[moduleName] = sandbox[moduleName] = moduleType;
+	}	
+}
 function disposeModules() {
     if (!modules) {
         if (process.send) {

ox_modules/module-db-dotnet.js
@@ -12,7 +12,8 @@ module.exports = function(argv, context, rs, logger, dispatcher) {
     var transactionName = null;
     
     // call ModuleInit
-	dispatcher.execute('db', 'moduleInit', argv); //Array.prototype.slice.call(
+	if (dispatcher)
+		dispatcher.execute('db', 'moduleInit', argv); //Array.prototype.slice.call(
 
     /**
     * @summary Stores DB connection string to be used by other methods. 

ox_modules/module-mob-fiber.js
@@ -47,13 +47,19 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
 	module.autoWait = false;
 
 	/**
-     * @summary Pauses test execution for given amount of seconds.
+     * @summary Initializes test settings and start a new Appium session.
      * @function init
-     * @param {String} platform - Android or iOS.
-	 * @param {String} appPackage - name of the package to run.
-	 * @param {String} appActivity - app activity for run initialization.
+     * @param {Json} caps - desired capabilities.
+	 * @param {String} url - Appium server url, including port.
      */
 	module.init = function(caps, url) {
+		var deviceName = argv['mob@deviceName'];
+		if (deviceName && deviceName.length > 0)
+			caps.deviceName = deviceName;
+		var deviceOS = argv['mob@deviceOS'];
+		if (deviceOS)
+			caps.platformName = deviceOS;
+		
 		var retval = invokeDriverCommandComplete("init", null, Array.prototype.slice.call(arguments));
 		isInitialized = true;
 	};
@@ -67,19 +73,34 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
     module.transaction = function (name) { 
         ctx._lastTransactionName = name;
     };
-	
+	/**
+     * @summary Sets context.
+     * @function setContext
+	 * @param {String} context - Context name (NATIVE_APP, WEBVIEW, etc.).
+     */
 	module.setContext = function(context) {
 		driver.context(context);
 	};
-
+	/**
+     * @summary Gets source code of the page.
+     * @function getSource
+     */
 	module.getSource = function() {
 		return driver.source();
 	}
-
+	/**
+     * @summary Injects a snippet of JavaScript into the page.
+     * @function execute
+	 * @param {String} js - The script to be executed.
+	 * @param {Object} elm - Element to be passed as parameter.
+     */
 	module.execute = function(js, elm) {
 		return driver.execute(js, elm);
 	}
-	
+	/**
+     * @summary Automatically sets context to the first available WEBVIEW.
+     * @function setWebViewContext
+     */
 	module.setWebViewContext = function() {
 		try {
 			var contexts = driver.contexts();
@@ -97,7 +118,10 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
             throw new AppiumError(e, null, null);
         }
 	}
-	
+	/**
+     * @summary Ends Appium session.
+     * @function dispose
+     */
 	module.dispose = function() {
 		if (driver && isInitialized) {
             //console.log(wdSync.current());
@@ -106,7 +130,10 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
 			//return retval;
 		}
 	}	
-	
+	/**
+     * @summary Take a screenshot of the current page.
+     * @function takeScreenshot
+     */
 	module.takeScreenshot = function () {
 		return driver.takeScreenshot();
 	};
@@ -127,11 +154,9 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
 		//return invokeDriverCommandComplete("swipe", locator, Array.prototype.slice.call(arguments));
 	};
     /**
-     * @summary Clicks on a widget.
+     * @summary Click on an element based on given locator.
      * @function click
-     * @param {String} locator - Widget locator. "id=" to search by ID or "//" to search by XPath.
-     * @param {Integer} wait - Time in seconds to wait for the widget.
-     * @param {Integer} pollrate - Time in seconds between polling intervals.
+     * @param {String} locator - Element's locator. "id=" to search by ID or "//" to search by XPath.
      */
     module.click = function(locator) { 
 		return invokeDriverCommandComplete("click", locator, Array.prototype.slice.call(arguments));

commit b47e1108ad837579b46b968837f3e4651139ba8a

Fixed regression introduced in 35b99e689

ox_modules/module-web-dotnet.js
@@ -302,7 +302,7 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
      */
     module.pause = function() { return handleStepResult(dispatcher.execute('web', 'pause', Array.prototype.slice.call(arguments))); };
     /**
-     * @summary Waits for a popup window to appear.
+     * @summary Waits for a window to appear.
      * @description <code>windowLocator</code> can be:
      *              <ul>
      *              <li><code>title=TITLE</code> - Wait for the first window which matches the 
@@ -311,12 +311,12 @@ module.exports = function (argv, context, rs, logger, dispatcher) {
      *              </li>
      *              <li>An empty string - Wait for any new window to appear.</li>
      *              </ul>
-     * @function waitForPopUp
+     * @function waitForWindow
      * @param {String} windowLocator - A window locator.
      * @param {Integer} timeout - A timeout in milliseconds, after which the action will return with 
      *                           an error.
      */
-    module.waitForPopUp = function() { return handleStepResult(dispatcher.execute('web', 'waitForPopUp', Array.prototype.slice.call(arguments))); };
+    module.waitForWindow = function() { return handleStepResult(dispatcher.execute('web', 'waitForWindow', Array.prototype.slice.call(arguments))); };
     /**
      * @summary Selects a frame within the current window.
      * @description Available frame locators: