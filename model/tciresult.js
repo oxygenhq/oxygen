ecfacb9fa990f20bc97c4592566b2666bc93e129
ndimer <nachum.dimer@gmail.com>
2016-11-11 12:44:00 +0100

    - Rerun test case if NAVIGATE_TIMEOUT error occurs
    - Indicate how many attempts to restart the test case were done
    - Allow to override file extension in parameter manager

--- model/tciresult.js
+++ model/tciresult.js
@@ -4,8 +4,9 @@
 module.exports = function () {
 	return {
 		_iterationNum: null,
+		_retries: null,			// indicates how many times some error occured and the test case was restarted
 		context: null,
-		steps: null,  // type of stepresult.js
+		steps: null,  			// type of stepresult.js
 		har: null
 	};
 };
