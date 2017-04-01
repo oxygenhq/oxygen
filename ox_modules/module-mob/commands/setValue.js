e6d4c50a73b63a5392a4e0c4884004c225721938
romovs <romovs@gmail.com>
2017-04-01 11:32:43 +0300

    Restore copyright headers

--- ox_modules/module-mob/commands/setValue.js
+++ ox_modules/module-mob/commands/setValue.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2015-2017 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 /**
  * @summary Sets element's value.
  * @function setValue
