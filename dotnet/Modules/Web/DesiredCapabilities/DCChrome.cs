d8b0cfecad020f2cef504af4e32138ca29d95507
romovs <romovs@gmail.com>
2017-04-08 12:53:01 +0300

    [dotnet] Remove ssl3 fullback in chrome desired capabilities

--- dotnet/Modules/Web/DesiredCapabilities/DCChrome.cs
+++ dotnet/Modules/Web/DesiredCapabilities/DCChrome.cs
@@ -16,11 +16,7 @@ namespace CloudBeat.Oxygen.Modules
     {
         public DesiredCapabilities Create(Capabilities caps) 
         {
-			//var dc = DesiredCapabilities.Chrome();
 			ChromeOptions options = new ChromeOptions();
-            // in Chrome 39+ SSLv3 fallback is disabled. we need to enable it back.
-            // https://groups.google.com/a/chromium.org/forum/#!topic/security-dev/Vnhy9aKM_l4
-            options.AddArgument("ssl-version-fallback-min=ssl3");
             if (caps != null && caps.ChromeBinary != null)
                 options.BinaryLocation = caps.ChromeBinary;
 			return (DesiredCapabilities)options.ToCapabilities();
