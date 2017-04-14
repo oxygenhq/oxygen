ff58bd2e209c4a5ec9a1b46ddd5b9a4f0ddaaf7b
romovs <romovs@gmail.com>
2017-04-14 15:03:07 +0300

    [mob] Add hideKeyboard

---
+++ ox_modules/module-mob/commands/hideKeyboard.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015-2017 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+/**
+ * @summary Hides device keyboard.
+ * @function hideKeyboard
+ * @param {String} strategy - Strategy to use for closing the keyboard ('tapOutside' or 'pressDone').
+ */
+module.exports = function(strategy) {
+    if (strategy && strategy === 'pressDone') {
+        return this._driver.hideDeviceKeyboard('pressKey', 'Done');
+    }
+    return this._driver.hideDeviceKeyboard(strategy);
+};
+
