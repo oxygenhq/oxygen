1df7d61f2fb83b3a3b72e37f1022487500807a7b
romovs <romovs@gmail.com>
2017-07-20 14:11:00 +0300

    Add mob.verifyTitle

---
+++ ox_modules/module-mob/commands/verifyTitle.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015-2017 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+/**
+ * @summary Verifies the page title.
+ * @function verifyTitle
+ * @param {String} pattern - Assertion text or pattern.
+ * @param {String=} message - Message to be displayed in case of assert failure.
+ * @platform web.hybrid
+ */
+const chai = require('chai');
+const assert = chai.assert;
+
+module.exports = function(pattern, message) {
+    var title = this._driver.getTitle();
+    this.helpers._assertArgumentNonEmptyString(pattern);
+    if (pattern.indexOf('regex:') == 0) {
+        var regex = new RegExp(pattern.substring('regex:'.length));
+        assert.match(title, regex, message);
+    } else {
+        assert.equal(title, pattern, message);
+    }
+};
+
