52878e5ba33306a5bed43873dcb2ab0962d65a0b
romovs <romovs@gmail.com>
2017-07-22 19:15:38 +0300

    Fix mob.swipe speed and update docs

--- ox_modules/module-mob/commands/swipe.js
+++ ox_modules/module-mob/commands/swipe.js
@@ -6,16 +6,18 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  */
+ 
 /**
- * @summary Performs a swipe.
+ * @summary Perform a swipe on the screen or an element.
  * @function swipe
- * @param {String} locator - Element locator.
- * @param {Integer} xoffset - Horizontal offset.
- * @param {Integer} yoffset - Vertical offset.
- * @param {Integer} speed - Time (in milliseconds) to spend performing the swipe.
+ * @param {String=} locator - Locator of the
+ * @param {Number} xoffset - Horizontal offset.
+ * @param {Number} yoffset - Vertical offset. Nagative value indicates swipe down and positive indicates swipe up direction.
+ * @param {Number=} speed - Time in seconds to spend performing the swipe. Default is 1 second.
+ * @for android, ios
 */
-var swipe = function(locator, xoffset, yoffset, speed) {
-    speed = typeof speed === 'number' ? speed : 100;
+module.exports = function(locator, xoffset, yoffset, speed) {
+    speed = typeof speed === 'number' ? speed : 1;
     if (arguments.length === 2 && typeof locator === 'number' && typeof xoffset === 'number') {
         xoffset = locator;
         yoffset = xoffset;
@@ -33,5 +35,3 @@ var swipe = function(locator, xoffset, yoffset, speed) {
     }
     return this._driver.swipe(xoffset, yoffset);
 };
-
-module.exports = swipe;
