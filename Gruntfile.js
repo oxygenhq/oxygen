1463e0f0af4001eb22a89d0ae6f230a5ffd2ff62
romovs <romovs@gmail.com>
2017-09-28 23:59:01 +0300

    Don't force XSLTransform compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,52 +8,21 @@
  */
  
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-sync');
-    grunt.loadNpmTasks('grunt-msbuild');
     grunt.loadNpmTasks('grunt-eslint');
     
     grunt.loadTasks('./tools/grunt-tasks');
 
     var defaultTasks = [];
     defaultTasks.push('eslint');
-    defaultTasks.push('msbuild:xsltransform');
-    defaultTasks.push('sync:main');
     
     grunt.registerTask('default', defaultTasks);
 
     grunt.initConfig({
-        sync: {
-            main: {
-                files: [
-                    { 
-                        expand: true, 
-                        cwd: 'dotnet/xsltransform/bin/Debug', src: ['XSLTransform.exe'], 
-                        dest: 'lib/reporters/html' 
-                    }
-                ], 
-                verbose: true
-            },
-        },
         eslint: {
             target: ['Gruntfile.js', 'lib/**/*.js', 'errors/**/*.js', 'model/**/*.js', 'ox_modules/**/*.js'],
             options: {
                 configFile: 'tools/.eslintrc.json'
             },
-        },
-        msbuild: {
-            xsltransform: {
-                src: ['dotnet/xsltransform/XSLTransform.csproj'],
-                options: {
-                    projectConfiguration: 'Debug',
-                    targets: ['Rebuild'],
-                    version: 12.0,
-                    maxCpuCount: 4,
-                    buildParameters: {
-                        WarningLevel: 2
-                    },
-                    verbosity: 'minimal'
-                }
-            }
         },
         dos2unix: {
             file: 'lib/cli.js'
