c057f864967582cbf1bfbee91603fad94e70d7ae
romovs <romovs@gmail.com>
2017-10-03 00:14:51 +0300

    Add mob.isCheckable, isChecked, isClickable, isSelected

---
+++ ox_modules/module-mob/commands/isSelected.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015-2017 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ * @summary Determines if an element is selected.
+ * @function isSelected
+ * @param {String} locator - Element locator.
+ * @return {Boolean} - true if element is selected. false otherwise.
+ * @for android
+ */
+module.exports = function(locator) {
+    this.helpers._assertLocator(locator);
+
+    if (typeof locator === 'object' && locator.getAttribute) {
+        return locator.getAttribute('selected');
+    }
+
+    locator = this.helpers.getWdioLocator(locator);
+    return this._driver.getAttribute(locator, 'selected');
+};
