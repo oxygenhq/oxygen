64e6f6d6d9bb5bd861fef8a98e3abc88df32f0fb
romovs <romovs@gmail.com>
2017-10-26 14:59:11 +0300

    Add wait argument to mob.getSmsText

--- ox_modules/module-mob/commands/getSmsText.js
+++ ox_modules/module-mob/commands/getSmsText.js
@@ -9,23 +9,26 @@
 
 /**
  * @summary Gets SMS text on Android phone.
- * @description <code>SmsPopup</code> application must be installed and running on the device to use this command.
+ * @description <code>SMS Popup by Adam K</code> application must be installed and running on the device to use this command.
  * @function getSmsText
+ * @param {Integer=} wait - Time in milliseconds to wait for sms popup. Default is 60 seconds.
  * @return {String} - SMS text.
  * @for android
 */
-module.exports = function() {
+module.exports = function(wait) {
     var locSms = 'id=net.everythingandroid.smspopup:id/messageTextView';
     var locSmsClose = 'id=net.everythingandroid.smspopup:id/button1';
 
-    this.waitForElement(locSms);
+    this.waitForElement(locSms, wait);
 
     var sms = this.findElement(locSms);
 
     var text = null;
-    if (sms){
+    if (sms) {
         text = sms.getText();
     }
+    // timeout doesn't really matter here since if popup is open close button will
+    // always exist
     this.waitForElement(locSmsClose);
     this.click(locSmsClose);
     return text;
