2a459019311ca7bf59b84c0f704ef85b55bae36f
romovs <romovs@gmail.com>
2017-12-21 15:51:26 +0200

    Add mob.alertAccept

---
+++ ox_modules/module-mob/commands/alertAccept.js
@@ -0,0 +1,8 @@
+/**
+ * @summary Accepts currently displayed alert.
+ * @function alertAccept
+ * @for android, ios, hybrid, web
+*/
+module.exports = function() {
+    return this.driver.alertAccept();
+};
