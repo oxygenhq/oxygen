226dc6db860110db3bea3b024cf89d1e9a11fda7
romovs <romovs@gmail.com>
2018-03-16 20:48:49 +0200

    Add web.waitForNotExist

---
+++ ox_modules/module-web/commands/waitForNotExist.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015-2018 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+ 
+/**
+ * @summary Waits for element to become unavailable in the DOM.
+ * @function waitForNotExist
+ * @param {String} locator - An element locator.
+ * @param {Integer=} timeout - Timeout in milliseconds. Default is 60 seconds.
+ */
+module.exports = function(locator, timeout) {
+    var wdloc = this.helpers.getWdioLocator(locator);
+    try {
+        this.driver.waitForExist(wdloc, (!timeout ? this.waitForTimeout : timeout), true);
+    } catch (e) {
+        console.log(JSON.stringify(e, null, 2));
+        if (e.type === 'WaitUntilTimeoutError') {
+            throw new this.OxError(this.errHelper.errorCode.ELEMENT_STILL_EXISTS);
+        }
+        throw e;
+    }
+};
