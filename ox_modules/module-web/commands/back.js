f337da311bb553c5eb30e80302216eb0faf53235
romovs <romovs@gmail.com>
2018-12-11 14:16:11 +0200

    Add web.back

---
+++ ox_modules/module-web/commands/back.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2015-2018 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+ 
+/**
+ * @summary Navigate backwards in the browser history.
+ * @function back
+ */
+module.exports = function() {
+    this.driver.back();
+};
