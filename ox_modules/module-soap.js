27cfe5c6edbb56a42597c8b3aecc65e782d9ea06
romovs <romovs@gmail.com>
2018-12-20 00:24:01 +0300

    Add authentication support from soap

--- ox_modules/module-soap.js
+++ ox_modules/module-soap.js
@@ -17,9 +17,41 @@ var errHelper = require('../errors/helper');
 module.exports = function() {
     var deasync = require('deasync');
     var soap = require('soap');
+    var auth = null;
 
     module._isInitialized = function() {
         return true;
+    };
+
+    /**
+     * @summary Sets Basic Authentication details to be used for connections with the service.
+     * @function authBasic
+     * @param {String} user - Username.
+     * @param {String} pass - Password.
+     */
+    module.authBasic = function(user, pass) {
+        auth = new soap.BasicAuthSecurity(user, pass);
+    };
+
+    /**
+     * @summary Sets Bearer Token Authentication details to be used for connections with the service.
+     * @function authBearer
+     * @param {String} token - Token.
+     */
+    module.authBearer = function(token) {
+        auth = new soap.BearerSecurity(token);
+    };
+
+    /**
+     * @summary Sets NTLM Authentication details to be used for connections with the service.
+     * @function authNTLM
+     * @param {String} user - Username.
+     * @param {String} pass - Password.
+     * @param {String} domain - Domain.
+     * @param {String} workstation - Workstation.
+     */
+    module.authNTLM = function(user, pass, domain, workstation) {
+        auth = new soap.NTLMSecurity(user, pass, domain, workstation);
     };
 
     /**
@@ -27,8 +59,14 @@ module.exports = function() {
      * @function get
      * @param {String} wsdlUrl - URL pointing to the WSDL XML.
      * @param {String} method - Method name (case sensitive).
-     * @param {Object=} args - Arguments.
+     * @param {Object=} args - Object containing the arguments.
      * @return {Object} The response object.
+     * @example <caption>[json] Example of args object for a method call which expects to receive 
+     * two arguments - foo and baz.</caption>
+     * {
+     *   foo: "bar",
+     *   baz: 1
+     * }
      */
     module.get = function(wsdlUrl, method, args) {
         var resultClient = null;
@@ -44,6 +82,8 @@ module.exports = function() {
                         soapMsg = err.message.substring(0, firstBreakIndex);
                     }
                     msg += ': ' + soapMsg;
+                } else if (err && (typeof err === 'string' || err instanceof String)) {
+                    msg += ': ' + err;
                 }
                 resultClient = new OxError(errHelper.errorCode.SOAP_ERROR, msg);
                 return;
@@ -51,12 +91,16 @@ module.exports = function() {
 
             resultClient = client;
 
-            if (typeof client[method] !== 'function') { 
+            if (typeof client[method] !== 'function') {
                 resultClient = new OxError(errHelper.errorCode.SOAP_ERROR, 'No method named ' + method + ' was found.');
                 return;
             }
 
-            client[method](args, (err, res) => { 
+            if (auth) {
+                client.setSecurity(auth);
+            }
+
+            client[method](args, (err, res) => {
                 if (err !== null) {
                     result = new OxError(errHelper.errorCode.SOAP_ERROR, err.root.Envelope.Body.Fault.faultstring);
                     return;
@@ -69,7 +113,7 @@ module.exports = function() {
         deasync.loopWhile(() => !resultClient);
         if (resultClient.type === errHelper.errorCode.SOAP_ERROR) {
             throw resultClient;
-        } 
+        }
 
         deasync.loopWhile(() => !result);
         if (result.type === errHelper.errorCode.SOAP_ERROR) {
@@ -98,18 +142,24 @@ module.exports = function() {
                         soapMsg = err.message.substring(0, firstBreakIndex);
                     }
                     msg += ': ' + soapMsg;
+                } else if (err && (typeof err === 'string' || err instanceof String)) {
+                    msg += ': ' + err;
                 }
                 resultClient = new OxError(errHelper.errorCode.SOAP_ERROR, msg);
                 return;
             }
 
-            resultClient = JSON.stringify(client.describe());
+            if (auth) {
+                client.setSecurity(auth);
+            }
+
+            resultClient = JSON.stringify(client.describe(), null, 2);
         });
 
         deasync.loopWhile(() => !resultClient);
         if (resultClient.type === errHelper.errorCode.SOAP_ERROR) {
             throw resultClient;
-        } 
+        }
 
         return resultClient;
     };
