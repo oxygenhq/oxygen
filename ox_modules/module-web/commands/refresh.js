59809c6541c1c6f3f5b42b633f0ef21801a4b9f0
romovs <romovs@gmail.com>
2019-01-15 16:11:08 +0300

    Add web.refresh

---
+++ ox_modules/module-web/commands/refresh.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2015-2018 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+ 
+/**
+ * @summary Causes the browser to reload the page.
+ * @function refresh
+ */
+module.exports = function() {
+    this.driver.refresh();
+};
