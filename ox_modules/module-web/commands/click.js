b24870d383bd3b2f71454731a26b8b8fb2aaf5dd
romovs <romovs@gmail.com>
2019-02-01 17:37:07 +0300

    Fix typo in if condition

--- ox_modules/module-web/commands/click.js
+++ ox_modules/module-web/commands/click.js
@@ -72,8 +72,8 @@ module.exports = function(locator) {
         if (e.message &&
             (e.message.includes('is not clickable at point')
                 || e.message === 'element not visible'
-                || e.message === 'element not interactable')
-                || e.message.startsWith('Element is not displayed')) {
+                || e.message === 'element not interactable'
+                || e.message.startsWith('Element is not displayed'))) {
             this.clickHidden(locator);
         } else {
             throw e;
