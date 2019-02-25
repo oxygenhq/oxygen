75be765b42b92dcc6475e84330cf7eedb15f8077
N. Dimer <ndimer@cloudbeat.io>
2019-02-24 23:11:19 -0800

    Add shell module.

---
+++ ox_modules/module-shell.js
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2015-present CloudBeat Limited
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*/
+
+/**
+* Provides methods for working with operating system shell.
+*/
+module.exports = function() {
+    module._isInitialized = function() {
+        return true;
+    };
+
+    module.exec = function(command, options = {}) {
+        const { spawnSync } = require('child_process');
+        options = { cwd: ox.ctx.cwd, ...options };
+        const result = spawnSync(command, options);
+        if (result.error) {
+            throw new Error(result.error);
+        }
+
+        if (result.output) {
+            return result.output.toString();
+        }
+        return null;
+    };
+
+    return module;
+};
