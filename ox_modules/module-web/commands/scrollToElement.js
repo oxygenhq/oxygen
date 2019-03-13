27cf6e1b6a9d4862dfce3f7e15d8cff84a4fc2a2
romovs <romovs@gmail.com>
2019-03-13 17:07:45 +0200

    Fix web.scrollToElement

--- ox_modules/module-web/commands/scrollToElement.js
+++ ox_modules/module-web/commands/scrollToElement.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2018 CloudBeat Limited
+ * Copyright (C) 2015-2019 CloudBeat Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 module.exports = function(locator, xoffset, yoffset) {
     var wdloc = this.helpers.getWdioLocator(locator);
     if (this.autoWait) {
-        this.waitForVisible(locator);
+        this.waitForExist(locator);
     }
     return this.driver.scroll(wdloc, xoffset, yoffset);
 };
