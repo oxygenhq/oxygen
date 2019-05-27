ea1786c6027e160ce0ec0c16d0ae4b9d2cd650b4
romovs <romovs@gmail.com>
2019-05-27 20:50:24 +0300

    Add web.getTitle

---
+++ ox_modules/module-web/commands/getTitle.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015-2019 CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+ 
+/**
+ * @summary Returns the title of the currently active window.
+ * @function getTitle
+ * @return {String} The page title.
+ */
+module.exports = function(locator) {
+    return this.driver.getTitle();
+};
