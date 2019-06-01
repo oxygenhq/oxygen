f0cff14b6901b275ac682bd77bf50823b5149a85
AM <amikh05@gmail.com>
2019-06-01 15:35:16 +0300

    add function for send keyboard to code

--- ox_modules/module-web/commands/sendKeyboardActions.js
+++ ox_modules/module-web/commands/sendKeyboardActions.js
@@ -4,131 +4,54 @@
  */
 
 /**
- * @summary Send a sequence of key strokes to the active window or element.
+ * @summary Send a sequence of key board strokes to the active window or element.
  * @description Refer to [Key Codes](https://w3c.github.io/webdriver/#keyboard-actions)
  *              for the list of supported raw keyboard key codes.
- * @function sendKeys with any value 
+ * @function sendKeyboardActions with any value from keyboard 
  * @param {String} value - Sequence of key strokes to send.
  * @example <caption>[javascript] Usage example</caption>
  * web.init();//Opens browser session.
- * web.open("www.yourwebsite.com");// Opens a website.
- * web.sendKeys(‘Enter’);//Types keys with value from keyboard.
+ * web.open("www.google.com");// Opens a website.
+ * web.type(id='tsf',"Paris");//Write a word in search line 
+ * web.sendKeyboardActions(‘Enter’);//Click on Types keys (Enter) with value from keyboard.
 */
 module.exports = function(value) {
-
-	var wdloc = this.helpers.getWdioLocator(locator);
-	if (this.autoWait) {
-		this.waitForVisible(locator);
-	}
-switch (value) {
-	case "Unidentified":
-		value = "\uE000";
-		break;
-	case "Cancel":
-		value = "\uE001";
-		break;
-	case "Help":
-		value = "\uE002";
-		break;
-	case "Backspace":
-		value = "\uE003";
-		break;
-	case "Tab":
-		value = "\uE004";
-		break;
-	case "Clear":
-		value = "\uE005";
-		break;
-	case "Return":
-		value = "\uE006";
-		break;
-    case "Enter":
-		value = "\uE007";
-		break;
-	case "Shift":
-		value = "\uE008";
-		break;
-	case "Control":
-		value = "\uE009";
-		break;
-	case "Alt":
-		value = "\uE00A";
-		break;
-	case "Pause":
-		value = "\uE00B";
-		break;
-	case "Escape":
-		value = "\uE00C";
-		break;
-	case "PageUp":
-		value = "\uE00E";
-		break;
-	case "PageDown":
-		value = "\uE00F";
-		break;
-	case "End":
-		value = "\uE010";
-		break;
-	case "Home":
-		value = "\uE011";
-		break;
-	case "ArrowLeft":
-		value = "\uE012";
-		break;
-	case "ArrowUp":
-		value = "\uE013";
-		break;
-	case "ArrowRight":
-		value = "\uE014";
-		break;
-	case "ArrowDown":
-		value = "\uE015";
-		break;
-	case "Insert":
-		value = "\uE016";
-		break;
-	case "Delete":
-		value = "\uE017";
-		break;
-	case "F1":
-		value = "\uE031";
-		break;
-	case "F2":
-		value = "\uE032";
-		break;
-	case "F3":
-		value = "\uE033";
-		break;
-	case "F4":
-		value = "\uE034";
-		break;
-	case "F5":
-		value = "\uE035";
-		break;
-	case "F6":
-		value = "\uE036";
-		break;
-	case "F7":
-		value = "\uE037";
-		break;
-	case "F8":
-		value = "\uE038";
-		break;
-	case "F9":
-		value = "\uE039";
-		break;
-	case "F10":
-		value = "\uE03A";
-		break;
-	case "F11":
-		value = "\uE03B";
-		break;
-	case "F12":
-		value = "\uE03C";
-		break;
-	default:
-		break;
-	}
-
-	return this.driver.keys(value);
+    var index=null;
+    switch(value){
+        case 'PageUp':
+            index='\uE054';
+            break;
+        case 'PageDown':
+            index='\uE055';
+            break;
+        case 'End':
+            index='\uE056';
+            break;
+        case 'Home':
+            index='\uE057';
+            break;
+        case 'ArrowLeft':
+            index='\uE058';
+            break;
+        case 'ArrowUp':
+            index='\uE059';
+            break;
+        case 'ArrowRight':
+            index='\uE05A';
+            break;
+        case 'ArrowDown':
+            index='\uE05B';
+            break;
+        case 'Insert':
+            index='\uE05C';
+            break;
+        case 'Delete':
+            index='\uE05D';
+            break;
+        case 'Enter':
+            index='\uE007';
+            break;
+    }
+    this.helpers.assertArgument(index);
+    this.driver.keys(index);
 };
