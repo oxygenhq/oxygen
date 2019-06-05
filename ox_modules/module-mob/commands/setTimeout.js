502540e923aa33fb6713371ff5edb4ae6a5978d7
N. Dimer <ndimer@cloudbeat.io>
2019-06-05 15:33:48 +0200

    [OX-324] Allow to set timeout (mob.setTimeout()) in 'mob' module.

---
+++ ox_modules/module-mob/commands/setTimeout.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015-present CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+ 
+/**
+ * @summary Specifies the amount of time that Oxygen will wait for actions to complete.
+ * @description This includes the `open` command, `waitFor*` commands, and all other commands which
+ *              wait for elements to appear or become visible before operating on them.  
+ *              If command wasn't able to complete within the specified period it will fail the
+ *              test.  
+ *              The default time-out is 60 seconds.
+ * @function setTimeout
+ * @param {Number} timeout - A time-out in milliseconds.
+ * @example <caption>[javascript] Usage example</caption>
+ * mob.init();//Opens browser session.
+ * mob.setTimeout(60000);//Sets the time out to amount of milliseconds .
+ */
+module.exports = function(timeout) {
+    this.helpers._assertArgumentTimeout(timeout, 'timeout');
+    return this.waitForTimeout = timeout;
+};
