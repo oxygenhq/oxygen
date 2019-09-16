0b05ab3ebd6d58cae34cc4a2e625ec39f659be4e
romovs <romovs@gmail.com>
2019-09-16 19:37:35 +0300

    Add web.isSelected

---
+++ ox_modules/module-web/commands/isSelected.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015-present CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ * @summary Determines if an element is selected.
+ * @function isSelected
+ * @param {String|Element} locator - Element locator.
+ * @param {Number=} timeout - Timeout in milliseconds. Default is 60 seconds.
+ * @return {Boolean} - true if element is selected. false otherwise.
+ * @example <caption>[javascript] Usage example</caption>
+ * web.init();
+ * web.open('http://www.wikipedia.org');
+ * var a = web.isSelected("id=Selection");
+ * if (a) {
+ *   ...
+ * } else {
+ *   ...
+ * }
+ */
+module.exports = function(locator, timeout) {
+    this.helpers.assertArgumentTimeout(timeout, 'timeout');
+
+    var el = this.helpers.getElement(locator, false, timeout);
+    return el.getAttribute('selected') == 'true';
+};
