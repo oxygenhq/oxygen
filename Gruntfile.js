cf283d1fb658cbbc41a2665e9a62a4d611e7759b
romovs <romovs@gmail.com>
2019-09-29 14:13:41 +0300

    Use eslint directly instead via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2017 CloudBeat Limited
+ * Copyright (C) 2015-present CloudBeat Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,22 +8,13 @@
  */
  
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-eslint');
-    
     grunt.loadTasks('./tools/grunt-tasks');
 
     var defaultTasks = [];
-    defaultTasks.push('eslint');
     
     grunt.registerTask('default', defaultTasks);
 
     grunt.initConfig({
-        eslint: {
-            target: ['Gruntfile.js', 'lib/**/*.js', 'errors/**/*.js', 'model/**/*.js', 'ox_modules/**/*.js'],
-            options: {
-                configFile: 'tools/.eslintrc.json'
-            },
-        },
         dos2unix: {
             file: 'lib/cli.js'
         },
