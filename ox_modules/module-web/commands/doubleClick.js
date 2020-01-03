0a6abdc9866d7f960bd1853c14e81b204376d066
romovs <romovs@gmail.com>
2020-01-03 12:44:45 +0200

    Add JS fallback for web.doubleClick

--- ox_modules/module-web/commands/doubleClick.js
+++ ox_modules/module-web/commands/doubleClick.js
@@ -19,7 +19,39 @@
  */
 module.exports = function(locator, timeout) {
     this.helpers.assertArgumentTimeout(timeout, 'timeout');
+    
+    try {
+        let el = this.helpers.getElement(locator, false, timeout);
 
-    var el = this.helpers.getElement(locator, true, timeout);
-    el.doubleClick();
+        // if the element is outside the viewport - try to scroll it into the view first
+        // on evetyhing except IE, because it doesn't work on IE
+        // taken from https://github.com/webdriverio/webdriverio/blob/master/packages/webdriverio/src/scripts/isElementClickable.js
+        // TODO: once WDIO updated to newer version which has isClickable, should simply use isClickable
+        if (this.caps.browserName !== 'internet explorer' && !el.isDisplayedInViewport()) {
+            el.scrollIntoView({ block: 'nearest', inline: 'nearest' });
+            if (!el.isDisplayedInViewport()) {
+                el.scrollIntoView({ block: 'nearest', inline: 'nearest' });
+            }
+        }
+        el.doubleClick();
+    } catch (e) {
+        // if element is not clickable, try clicking it using JS injection
+        if (e.message &&
+            (e.message.includes('is not clickable at point')
+                || e.message === 'element not visible'
+                || e.message === 'element not interactable'                     // chrome
+                || e.message === 'Element is not displayed'                     // ie
+                || e.message.includes('could not be scrolled into view'))) {    // firefox
+            let el = this.helpers.getElement(locator, false, timeout);
+            // NOTE: adding comments inside the passed function is not allowed!
+            /*global document*/
+            this.driver.execute(function (domEl) {
+                var clckEv = document.createEvent('MouseEvent');
+                clckEv.initEvent('dblclick', true, true);
+                domEl.dispatchEvent(clckEv);
+            }, el);
+        } else {
+            throw e;
+        }
+    }
 };
