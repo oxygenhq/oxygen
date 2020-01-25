64fb1983717c3ad29c9bcd2346d997e61a524a05
N. Dimer <ndimer@cloudbeat.io>
2020-01-25 10:11:16 +0100

    Expose cli-util file.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as Runners from './runners';
 import Launcher from './lib/launcher';
 import ReportAggregator from './reporter/ReportAggregator';
 import util from './lib/util';
+import * as cliutil from './lib/cli-util';
 import moduleRequire from './lib/moduleRequire';
 
 export {
@@ -9,5 +10,6 @@ export {
     Launcher,
     ReportAggregator,
     util,
+    cliutil,
     moduleRequire
 };
