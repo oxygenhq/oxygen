55d13aaa383d604551ae2328da8bf5959a636ca8
romovs <romovs@gmail.com>
2020-05-20 10:44:02 +0300

    Improve mob.scrollIntoView docs

--- src/ox_modules/module-mob/commands/scrollIntoView.js
+++ src/ox_modules/module-mob/commands/scrollIntoView.js
@@ -11,19 +11,25 @@
  * @summary Scrolls the page or a container element to the location of the specified element.
  * @function scrollIntoView
  * @param {String|Element} locator - An element locator.
- * @param {Boolean=} alignToTop - If true, the top of the element will be aligned to the top of the 
- *                                visible area of the scrollable ancestor. This is the default.
- *                                If false, the bottom of the element will be aligned to the bottom 
- *                                of the visible area of the scrollable ancestor. 
+ * @param {Boolean|Object=} options - If `true`, the top of the element will be aligned to the top of the 
+ * visible area of the scrollable ancestor. This is the default.  
+ * If `false`, the bottom of the element will be aligned to the bottom of the visible area of the 
+ * scrollable ancestor.  
+ * This parameter can also accept an `options` object. See the usage example above.
  * @param {Number=} timeout - Timeout in milliseconds. Default is 60 seconds.
  * @for hybrid, web
  * @example <caption>[javascript] Usage example</caption>
- * mob.init(caps);//Starts a mobile session and opens app from desired capabilities
- * mob.scrollIntoView('id=bottomPanel',true);//Scrolls the element into the visible area of the browser window.
+ * mob.scrollIntoView('id=bottomPanel', true);
+ * // or
+ * mob.scrollIntoView('id=bottomPanel', {
+ *   behavior: 'auto', // Optional. Defines the transition animation: `auto` or `smooth`. Defaults to `auto`.
+ *   block: 'start',   // Optional. Defines vertical alignment - `start`, `center`, `end`, `nearest`. Defaults to `start`.
+ *   inline: 'start'   // Optional. Defines horizontal alignment - `start`, `center`, `end`, `nearest`. Defaults to `start`.
+ * });
 */
-module.exports = function(locator, alignToTop = true, timeout) {
+module.exports = function(locator, options = true, timeout) {
     this.helpers.assertArgumentTimeout(timeout, 'timeout');
 
     var el = this.helpers.getElement(locator, false, timeout);
-    el.scrollIntoView(alignToTop);
+    el.scrollIntoView(options);
 };
