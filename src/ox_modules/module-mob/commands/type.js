dccd5b38b33a6754173b1fd70fe8f9070008e079
Savka Oleg <savkaoleg1996@gmail.com>
2020-05-27 15:20:05 +0300

    add mob.type support for native apps

--- src/ox_modules/module-mob/commands/type.js
+++ src/ox_modules/module-mob/commands/type.js
@@ -27,7 +27,31 @@ module.exports = function(locator, value, timeout) {
     var el = this.helpers.getElement(locator, true, timeout);
 
     try {
-        el.setValue(value.toString());
+
+        if(
+            this.driver &&
+            this.driver.capabilities &&
+            this.driver.capabilities.entityType &&
+            this.driver.capabilities.entityType === 'appium_driver'            
+        ) {
+            let saveValue;
+
+            if(value){
+                if(Array.isArray(value)){
+                    saveValue = value;
+                } else if(typeof value === 'string'){
+                    saveValue = [value.toString()];
+                } else if(value.toString){
+                    saveValue = [value.toString()];
+                }
+            }
+
+            // native app
+            el.sendKeys(saveValue);
+        } else {
+            // remote_web_driver (mob browser)
+            el.setValue(value.toString());
+        }
     } catch (e) {
         if(
             e && 
