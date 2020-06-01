dd759b5ebc147175548ca68ea942880a1c8fcfcc
Savka Oleg <savkaoleg1996@gmail.com>
2020-06-01 14:24:17 +0300

    Ignore getTimeouts for perfecto

--- src/ox_modules/utils.js
+++ src/ox_modules/utils.js
@@ -180,7 +180,15 @@ module.exports = {
         } else if (this.driver.getTimeouts) {
             // chrome >= 75
             try {
-                timeouts = this.driver.getTimeouts();
+                if(
+                    this.driver.capabilities &&
+                    this.driver.capabilities.stopUrl &&
+                    this.driver.capabilities.stopUrl.includes('perfectomobile.com')
+                ){
+                    //ignore
+                } else {
+                    timeouts = this.driver.getTimeouts();
+                }
             } catch(e){
                 // fails on perfecto mobile
                 console.log('getTimeouts error', e);
