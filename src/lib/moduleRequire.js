73f223d88231b06a5fd9db6bdc527fbffa711685
romovs <romovs@gmail.com>
2020-08-19 19:02:55 +0300

    Use standard way for requiring cfg files

--- src/lib/moduleRequire.js
+++ src/lib/moduleRequire.js
@@ -1,23 +1,9 @@
 var decache = require('decache');
-var fs = require('fs');
-
-function requireFromString(src, filename) {
-    var Module = module.constructor;
-    var m = new Module();
-    m._compile(src, filename);
-    return m.exports;
-}
-
-function orgRequire(path) {
-    var text = fs.readFileSync(path,'utf8');
-    return requireFromString(text, path);
-}
 
 export default function moduleRequire(moduleName) {
     try {
         decache(moduleName);
-        var result = orgRequire(moduleName);
-        return result;
+        return require(moduleName);
     } catch (e) {
         console.log('moduleRequire e', e);
         throw e;
