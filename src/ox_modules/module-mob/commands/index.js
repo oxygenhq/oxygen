658277712b99355ef907e0d97245428c3ca1e109
romovs <romovs@gmail.com>
2020-09-06 13:36:05 +0300

    Simplify command import-export

--- src/ox_modules/module-mob/commands/index.js
+++ src/ox_modules/module-mob/commands/index.js
@@ -1,195 +1,65 @@
-import _alertAccept from './alertAccept';
-export const alertAccept = _alertAccept;
-
-import _alertDismiss from './alertDismiss';
-export const alertDismiss = _alertDismiss;
-
-import _assertText from './assertText';
-export const assertText = _assertText;
-
-import _assertTitle from './assertTitle';
-export const assertTitle = _assertTitle;
-
-import _assertValue from './assertValue';
-export const assertValue = _assertValue;
-
-import _back from './back';
-export const back = _back;
-
-import _clear from './clear';
-export const clear = _clear;
-
-import _click from './click';
-export const click = _click;
-
-import _clickHidden from './clickHidden';
-export const clickHidden = _clickHidden;
-
-import _clickLong from './clickLong';
-export const clickLong = _clickLong;
-
-import _clickMultipleTimes from './clickMultipleTimes';
-export const clickMultipleTimes = _clickMultipleTimes;
-
-import _closeApp from './closeApp';
-export const closeApp = _closeApp;
-
-import _dragAndDrop from './dragAndDrop';
-export const dragAndDrop = _dragAndDrop;
-
-import _enableNetwork from './enableNetwork';
-export const enableNetwork = _enableNetwork;
-
-import _execute from './execute';
-export const execute = _execute;
-
-import _findElement from './findElement';
-export const findElement = _findElement;
-
-import _findElements from './findElements';
-export const findElements = _findElements;
-
-import _getAlertText from './getAlertText';
-export const getAlertText = _getAlertText;
-
-import _getAppiumLogs from './getAppiumLogs';
-export const getAppiumLogs = _getAppiumLogs;
-
-import _getCurrentActivity from './getCurrentActivity';
-export const getCurrentActivity = _getCurrentActivity;
-
-import _getCurrentPackage from './getCurrentPackage';
-export const getCurrentPackage = _getCurrentPackage;
-
-import _getDeviceLogs from './getDeviceLogs';
-export const getDeviceLogs = _getDeviceLogs;
-
-import _getDeviceTime from './getDeviceTime';
-export const getDeviceTime = _getDeviceTime;
-
-import _getLocation from './getLocation';
-export const getLocation = _getLocation;
-
-import _getSource from './getSource';
-export const getSource = _getSource;
-
-import _getText from './getText';
-export const getText = _getText;
-
-import _getTitle from './getTitle';
-export const getTitle = _getTitle;
-
-import _getUrl from './getUrl';
-export const getUrl = _getUrl;
-
-import _getValue from './getValue';
-export const getValue = _getValue;
-
-import _hideKeyboard from './hideKeyboard';
-export const hideKeyboard = _hideKeyboard;
-
-import _index from './index';
-export const index = _index;
-
-import _installApp from './installApp';
-export const installApp = _installApp;
-
-import _isAppInstalled from './isAppInstalled';
-export const isAppInstalled = _isAppInstalled;
-
-import _isCheckable from './isCheckable';
-export const isCheckable = _isCheckable;
-
-import _isChecked from './isChecked';
-export const isChecked = _isChecked;
-
-import _isClickable from './isClickable';
-export const isClickable = _isClickable;
-
-import _isExist from './isExist';
-export const isExist = _isExist;
-
-import _isSelected from './isSelected';
-export const isSelected = _isSelected;
-
-import _isVisible from './isVisible';
-export const isVisible = _isVisible;
-
-import _isWebViewContext from './isWebViewContext';
-export const isWebViewContext = _isWebViewContext;
-
-import _launchApp from './launchApp';
-export const launchApp = _launchApp;
-
-import _open from './open';
-export const open = _open;
-
-import _pause from './pause';
-export const pause = _pause;
-
-import _removeApp from './removeApp';
-export const removeApp = _removeApp;
-
-import _resetApp from './resetApp';
-export const resetApp = _resetApp;
-
-import _scrollIntoElement from './scrollIntoElement';
-export const scrollIntoElement = _scrollIntoElement;
-
-import _scrollIntoView from './scrollIntoView';
-export const scrollIntoView = _scrollIntoView;
-
-import _selectFrame from './selectFrame';
-export const selectFrame = _selectFrame;
-
-import _sendKeys from './sendKeys';
-export const sendKeys = _sendKeys;
-
-import _setContext from './setContext';
-export const setContext = _setContext;
-
-import _setNativeContext from './setNativeContext';
-export const setNativeContext = _setNativeContext;
-
-import _setTimeout from './setTimeout';
-export const setTimeout = _setTimeout;
-
-import _setWebViewContext from './setWebViewContext';
-export const setWebViewContext = _setWebViewContext;
-
-import _shake from './shake';
-export const shake = _shake;
-
-import _smsClickLink from './smsClickLink';
-export const smsClickLink = _smsClickLink;
-
-import _smsGetText from './smsGetText';
-export const smsGetText = _smsGetText;
-
-import _swipe from './swipe';
-export const swipe = _swipe;
-
-import _swipeElement from './swipeElement';
-export const swipeElement = _swipeElement;
-
-import _swipeScreen from './swipeScreen';
-export const swipeScreen = _swipeScreen;
-
-import _takeScreenshot from './takeScreenshot';
-export const takeScreenshot = _takeScreenshot;
-
-import _tap from './tap';
-export const tap = _tap;
-
-import _type from './type';
-export const type = _type;
-
-import _unlockPattern from './unlockPattern';
-export const unlockPattern = _unlockPattern;
-
-import _waitForExist from './waitForExist';
-export const waitForExist = _waitForExist;
-
-import _waitForVisible from './waitForVisible';
-export const waitForVisible = _waitForVisible;
-
+export {default as alertAccept} from './alertAccept';
+export {default as alertDismiss} from './alertDismiss';
+export {default as assertText} from './assertText';
+export {default as assertTitle} from './assertTitle';
+export {default as assertValue} from './assertValue';
+export {default as back} from './back';
+export {default as clear} from './clear';
+export {default as click} from './click';
+export {default as clickHidden} from './clickHidden';
+export {default as clickLong} from './clickLong';
+export {default as clickMultipleTimes} from './clickMultipleTimes';
+export {default as closeApp} from './closeApp';
+export {default as dragAndDrop} from './dragAndDrop';
+export {default as enableNetwork} from './enableNetwork';
+export {default as execute} from './execute';
+export {default as findElement} from './findElement';
+export {default as findElements} from './findElements';
+export {default as getAlertText} from './getAlertText';
+export {default as getAppiumLogs} from './getAppiumLogs';
+export {default as getCurrentActivity} from './getCurrentActivity';
+export {default as getCurrentPackage} from './getCurrentPackage';
+export {default as getDeviceLogs} from './getDeviceLogs';
+export {default as getDeviceTime} from './getDeviceTime';
+export {default as getLocation} from './getLocation';
+export {default as getSource} from './getSource';
+export {default as getText} from './getText';
+export {default as getTitle} from './getTitle';
+export {default as getUrl} from './getUrl';
+export {default as getValue} from './getValue';
+export {default as hideKeyboard} from './hideKeyboard';
+export {default as index} from './index';
+export {default as installApp} from './installApp';
+export {default as isAppInstalled} from './isAppInstalled';
+export {default as isCheckable} from './isCheckable';
+export {default as isChecked} from './isChecked';
+export {default as isClickable} from './isClickable';
+export {default as isExist} from './isExist';
+export {default as isSelected} from './isSelected';
+export {default as isVisible} from './isVisible';
+export {default as isWebViewContext} from './isWebViewContext';
+export {default as launchApp} from './launchApp';
+export {default as open} from './open';
+export {default as pause} from './pause';
+export {default as removeApp} from './removeApp';
+export {default as resetApp} from './resetApp';
+export {default as scrollIntoElement} from './scrollIntoElement';
+export {default as scrollIntoView} from './scrollIntoView';
+export {default as selectFrame} from './selectFrame';
+export {default as sendKeys} from './sendKeys';
+export {default as setContext} from './setContext';
+export {default as setNativeContext} from './setNativeContext';
+export {default as setTimeout} from './setTimeout';
+export {default as setWebViewContext} from './setWebViewContext';
+export {default as shake} from './shake';
+export {default as smsClickLink} from './smsClickLink';
+export {default as smsGetText} from './smsGetText';
+export {default as swipe} from './swipe';
+export {default as swipeElement} from './swipeElement';
+export {default as swipeScreen} from './swipeScreen';
+export {default as takeScreenshot} from './takeScreenshot';
+export {default as tap} from './tap';
+export {default as type} from './type';
+export {default as unlockPattern} from './unlockPattern';
+export {default as waitForExist} from './waitForExist';
+export {default as waitForVisible} from './waitForVisible';
--- src/ox_modules/module-win/commands/index.js
+++ src/ox_modules/module-win/commands/index.js
@@ -1,105 +1,35 @@
-import _assertText from './assertText';
-export const assertText = _assertText;
-
-import _assertTitle from './assertTitle';
-export const assertTitle = _assertTitle;
-
-import _assertValue from './assertValue';
-export const assertValue = _assertValue;
-
-import _back from './back';
-export const back = _back;
-
-import _clear from './clear';
-export const clear = _clear;
-
-import _click from './click';
-export const click = _click;
-
-import _clickLong from './clickLong';
-export const clickLong = _clickLong;
-
-import _clickMultipleTimes from './clickMultipleTimes';
-export const clickMultipleTimes = _clickMultipleTimes;
-
-import _findElement from './findElement';
-export const findElement = _findElement;
-
-import _findElements from './findElements';
-export const findElements = _findElements;
-
-import _getAppiumLogs from './getAppiumLogs';
-export const getAppiumLogs = _getAppiumLogs;
-
-import _getCurrentWindowHandle from './getCurrentWindowHandle';
-export const getCurrentWindowHandle = _getCurrentWindowHandle;
-
-import _getLocation from './getLocation';
-export const getLocation = _getLocation;
-
-import _getSource from './getSource';
-export const getSource = _getSource;
-
-import _getText from './getText';
-export const getText = _getText;
-
-import _getValue from './getValue';
-export const getValue = _getValue;
-
-import _getWindowHandles from './getWindowHandles';
-export const getWindowHandles = _getWindowHandles;
-
-import _index from './index';
-export const index = _index;
-
-import _isCheckable from './isCheckable';
-export const isCheckable = _isCheckable;
-
-import _isChecked from './isChecked';
-export const isChecked = _isChecked;
-
-import _isClickable from './isClickable';
-export const isClickable = _isClickable;
-
-import _isExist from './isExist';
-export const isExist = _isExist;
-
-import _isSelected from './isSelected';
-export const isSelected = _isSelected;
-
-import _isVisible from './isVisible';
-export const isVisible = _isVisible;
-
-import _open from './open';
-export const open = _open;
-
-import _pause from './pause';
-export const pause = _pause;
-
-import _rightClick from './rightClick';
-export const rightClick = _rightClick;
-
-import _selectWindow from './selectWindow';
-export const selectWindow = _selectWindow;
-
-import _sendKeys from './sendKeys';
-export const sendKeys = _sendKeys;
-
-import _setTimeout from './setTimeout';
-export const setTimeout = _setTimeout;
-
-import _takeScreenshot from './takeScreenshot';
-export const takeScreenshot = _takeScreenshot;
-
-import _tap from './tap';
-export const tap = _tap;
-
-import _type from './type';
-export const type = _type;
-
-import _waitForExist from './waitForExist';
-export const waitForExist = _waitForExist;
-
-import _waitForVisible from './waitForVisible';
-export const waitForVisible = _waitForVisible;
-
+export {default as assertText} from './assertText';
+export {default as assertTitle} from './assertTitle';
+export {default as assertValue} from './assertValue';
+export {default as back} from './back';
+export {default as clear} from './clear';
+export {default as click} from './click';
+export {default as clickLong} from './clickLong';
+export {default as clickMultipleTimes} from './clickMultipleTimes';
+export {default as findElement} from './findElement';
+export {default as findElements} from './findElements';
+export {default as getAppiumLogs} from './getAppiumLogs';
+export {default as getCurrentWindowHandle} from './getCurrentWindowHandle';
+export {default as getLocation} from './getLocation';
+export {default as getSource} from './getSource';
+export {default as getText} from './getText';
+export {default as getValue} from './getValue';
+export {default as getWindowHandles} from './getWindowHandles';
+export {default as index} from './index';
+export {default as isCheckable} from './isCheckable';
+export {default as isChecked} from './isChecked';
+export {default as isClickable} from './isClickable';
+export {default as isExist} from './isExist';
+export {default as isSelected} from './isSelected';
+export {default as isVisible} from './isVisible';
+export {default as open} from './open';
+export {default as pause} from './pause';
+export {default as rightClick} from './rightClick';
+export {default as selectWindow} from './selectWindow';
+export {default as sendKeys} from './sendKeys';
+export {default as setTimeout} from './setTimeout';
+export {default as takeScreenshot} from './takeScreenshot';
+export {default as tap} from './tap';
+export {default as type} from './type';
+export {default as waitForExist} from './waitForExist';
+export {default as waitForVisible} from './waitForVisible';
