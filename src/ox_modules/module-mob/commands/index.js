ff6d274d43c5d5bc5bb19d653d0f1915f9848f04
Oleg Savka <savkaoleg1996@gmail.com>
2020-10-15 21:19:36 +0300

    add_waitForOpen

--- src/ox_modules/module-mob/commands/index.js
+++ src/ox_modules/module-mob/commands/index.js
@@ -42,6 +42,7 @@ export {default as isVisible} from './isVisible';
 export {default as isWebViewContext} from './isWebViewContext';
 export {default as launchApp} from './launchApp';
 export {default as open} from './open';
+export {default as waitForOpen} from './waitForOpen';
 export {default as pause} from './pause';
 export {default as removeApp} from './removeApp';
 export {default as resetApp} from './resetApp';
