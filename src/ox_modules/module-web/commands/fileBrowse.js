d2b4d42a7f760e98d1b3cee32f4d654a563610b8
Savka Oleg <savkaoleg1996@gmail.com>
2021-03-21 14:28:24 +0200

    web.fileBrowse fix

--- src/ox_modules/module-web/commands/fileBrowse.js
+++ src/ox_modules/module-web/commands/fileBrowse.js
@@ -18,20 +18,52 @@
  * web.open("www.yourwebsite.com");// Opens a website.
  * web.fileBrowse("id=ProfilePicture","C:\\picture.jpg");//Uploads a file to an element.
  */
+
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async function(locator, filepath, timeout) {
     this.helpers.assertArgumentNonEmptyString(filepath, 'filepath');
     this.helpers.assertArgumentTimeout(timeout, 'timeout');
 
-    var el = await this.helpers.getElement(locator, false, timeout);
-
-    var remoteFilePath = await this.driver.uploadFile(filepath);
-
+    const el = await this.helpers.getElement(locator, false, timeout);
     try {
-        await el.setValue(remoteFilePath);
-    } catch (e) {
-        if (e.name === 'invalid element state') {
-            throw new this.OxError(this.errHelper.errorCode.ELEMENT_STATE_ERROR, e.message);
+        // make an asynchronous call using archiver 3rd party library supporting promises
+        const remoteFilePath = await this.driver.call(() => {
+            return new Promise( (resolve, reject) => {
+                let zipData = [];
+                // require in top level produce Error : Cannot read property 'objectMode' of undefined
+                const archiver = require('archiver');
+                const source = fs.createReadStream(filepath);
+                const name = path.basename(filepath);
+
+                archiver('zip')
+                    .on('error', (err) => reject(err))
+                    .on('data', (data) => {
+                        zipData.push(data);
+                    })
+                    .on('end', () => {
+                        this.driver.file(Buffer.concat(zipData).toString('base64')).then(resolve, reject);
+                    })
+                    .append(source, { name: name })
+                    .finalize((err) => {
+                        if (err) {
+                            reject(err);
+                        }
+                    });
+            });
+        });
+
+        try {
+            // set local temp file path into file input
+            await el.setValue(remoteFilePath);
+        } catch (e) {
+            if (e.name === 'invalid element state') {
+                throw new this.OxError(this.errHelper.errorCode.ELEMENT_STATE_ERROR, e.message);
+            }
+            throw e;
         }
-        throw e;
+    } catch (e) {
+        throw new this.OxError(this.errHelper.errorCode.FILE_BROWSE_ERROR, e.message);
     }
 };
