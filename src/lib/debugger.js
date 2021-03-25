1fcc49bb77a316d2822b988d4da9f24c1f1bbd87
Savka Oleg <savkaoleg1996@gmail.com>
2021-03-25 12:44:34 +0200

    IDE-B7 Cannot read property 'fileLineNumbersLength' of undefined

--- src/lib/debugger.js
+++ src/lib/debugger.js
@@ -584,6 +584,8 @@ export default class Debugger extends EventEmitter {
 
                                             if (
                                                 brFileName === breakpointData.fileName &&
+                                                possibleBreakpointData &&
+                                                typeof possibleBreakpointData.fileLineNumbersLength !== 'undefined' &&
                                                 (
                                                     breakpointData.lineNumber+1 === br.origin.lineNumber ||
                                                     br.origin.lineNumber+1 === possibleBreakpointData.fileLineNumbersLength
@@ -613,7 +615,7 @@ export default class Debugger extends EventEmitter {
 
                         if (
                             possibleBreakpointData &&
-                            possibleBreakpointData.fileLineNumbersLength &&
+                            typeof possibleBreakpointData.fileLineNumbersLength !== 'undefined' &&
                             breakpointData.lineNumber+1 >= possibleBreakpointData.fileLineNumbersLength
                         ) {
                             breakpointData.lineNumber = possibleBreakpointData.fileLineNumbersLength - 2;
@@ -637,7 +639,11 @@ export default class Debugger extends EventEmitter {
 
                                             item.file = transformToIDEStyle(item.file);
 
-                                            if (item && item.file === breakpointError.fileName) {
+                                            if (
+                                                item &&
+                                                item.file === breakpointError.fileName &&                                                possibleBreakpointData &&
+                                                typeof item.fileLineNumbersLength !== 'undefined'
+                                            ) {
                                                 let line;
 
                                                 if (breakpointError.line >= item.fileLineNumbersLength) {
