061a03353d1ad003542295f2fa7fd84c3664ead3
Savka Oleg <savkaoleg1996@gmail.com>
2021-06-18 09:42:12 +0300

    OI-1146 ([Menora] Add ability to use module csv-parse in the IDE) - implement as methods in "utils" module.

--- src/ox_modules/module-utils.js
+++ src/ox_modules/module-utils.js
@@ -16,6 +16,8 @@ const MODULE_NAME = 'utils';
 import OxygenModule from '../core/OxygenModule';
 import utils from './utils';
 const deasync = require('deasync');
+import OxError from '../errors/OxygenError';
+import errorHelper from '../errors/helper';
 
 export default class UtilsModule extends OxygenModule {
     constructor(options, context, rs, logger, modules, services) {
@@ -41,5 +43,65 @@ export default class UtilsModule extends OxygenModule {
     pause(ms) {
         utils.assertArgumentNumberNonNegative(ms, 'ms');
         deasync.sleep(ms);
+    }
+
+    /**
+     * @summary Reads data from csv file
+     * @function readCsv
+     * @param {String} filePath - Absolute path to file
+     * @param {Object=} options - Options (https://csv.js.org/parse/options/)
+     */
+    readCsv(filePath, options = {}) {
+        const parse = require('csv-parse/lib/sync');
+        var fs = require('fs');
+        var path = require('path');
+
+        var ext = path.extname(filePath);
+        if (ext !== '.csv' && ext !== '.txt') {
+            throw new OxError(errorHelper.errorCode.CSV_ERROR, 'Unsupported file extension: ' + ext);
+        }
+
+        var data;
+        try {
+            data = fs.readFileSync(filePath, 'utf8');
+        } catch (e) {
+            throw new OxError(errorHelper.errorCode.CSV_ERROR, 'Unable to read parameters file: ' + e);
+        }
+
+        // CSVs produced by OS X Excel have header terminated with 0d0d0a
+        data = data.replace(/\r\r/g, '\r');
+
+        // strip BOM for UTF-8
+        var bom = false;
+        // catches 0xEFBBBF (UTF-8 BOM) because the buffer-to-string
+        // conversion translates it to 0xFEFF (UTF-16 BOM)
+        if (data.charCodeAt(0) === 0xFEFF) {
+            bom = true;
+        }
+
+        try {
+            var table = parse(data, { bom: bom, columns: true, trim: true, ...options });
+        } catch (e) {
+            throw new OxError(errorHelper.errorCode.CSV_ERROR, e.message);
+        }
+
+        return table;
+    }
+
+    /**
+     * @summary Writes data into csv file
+     * @function writeCsv
+     * @param {String} filePath - Absolute path to file
+     * @param {Array} data - CSV data in format [{column_name_1: 'foo', column_name_2: 'bar'}]
+     * @param {Object=} options - Options (https://github.com/anton-bot/objects-to-csv#async-todiskfilename-options)
+     */
+    async writeCsv(filePath, data, options = {}) {
+        try {
+            const ObjectsToCsv = require('objects-to-csv');
+            const csv = new ObjectsToCsv(data);
+            await csv.toDisk(filePath, { allColumns: true, ...options });
+        } catch (e) {
+            throw new OxError(errorHelper.errorCode.CSV_ERROR, e.message);
+        }
     }
 }
