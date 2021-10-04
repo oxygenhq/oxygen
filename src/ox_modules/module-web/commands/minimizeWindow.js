656772b92861f35ed3ec924aae162322823f2006
Savka Oleg <savkaoleg1996@gmail.com>
2021-10-04 16:47:04 +0300

    Add maximizeWindow, minimizeWindow, fullscreenWindow

---
+++ src/ox_modules/module-web/commands/minimizeWindow.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015-present CloudBeat Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ * @summary Minimize Window.
+ * @function minimizeWindow
+ * @example <caption>[javascript] Usage example</caption>
+ * web.init();//Opens browser session.
+ * web.minimizeWindow();
+ */
+module.exports = async function() {
+    await this.driver.minimizeWindow();
+};
